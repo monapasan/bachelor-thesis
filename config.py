6aec55a796ce47ce28d135bf753f5937470d640a
monapasan <oleg.yarin@gmail.com>
2017-05-10 15:55:09 +0200

    clean up and comment configuration object

--- config.py
+++ config.py
@@ -3,20 +3,27 @@ class Config(object):
     bandwidth = win_size**2
     batch_size = 32
     eval_batch_size = 50
+    # standard deviation for
+    # the gaussian distribution
+    # when added to location
     loc_std = 0.22
     # zise of MNIST image, 28px x 28px
     original_size = 28
     # number of channels in the image, MNIST has 1 channel, i.e. white channel
     num_channels = 1
-    depth = 1
-    sensor_size = win_size**2 * depth
+    #  number of patches which should be extracted in a glimpse
+    n_patches = 1
+    sensor_size = win_size**2 * n_patches
     minRadius = 8
+    # Size of parameters for Glimpse network
     hg_size = hl_size = 128
     g_size = 256
+
     cell_output_size = 256
+    # location shape --> (x, y)
     loc_dim = 2
     cell_size = 256
-    cell_out_size = cell_size
+
     num_glimpses = 6
     num_classes = 10
     max_grad_norm = 5.
